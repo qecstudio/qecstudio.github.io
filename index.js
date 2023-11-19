a80ab5842a7f8620979901031fb2c069d48130df
Bo Chen <qecstudio@gmail.com>
2023-11-18 20:50:46 -0500

    Add Hello World

---
+++ index.js
@@ -0,0 +1,56 @@
+// 'use strict';
+
+// // const puppeteer = require('puppeteer');
+
+// import puppeteer from 'puppeteer';
+
+// (async () => {
+//   const browser = await puppeteer.launch({headless: 'new'});
+//   const page = await browser.newPage();
+//   await page.goto('https://finviz.com/news.ashx', {
+//     waitUntil: 'networkidle2',
+//   });
+//   // page.pdf() is currently supported only in headless mode.
+//   // @see https://bugs.chromium.org/p/chromium/issues/detail?id=753118
+//   await page.pdf({
+//     path: 'hn.pdf',
+//     format: 'letter',
+//   });
+
+//   await browser.close();
+// })();
+
+
+
+// let name = 'Log';
+
+// console.log('My name is '+ name);
+
+// // window.onload = getlink;
+
+
+//     let links = document.getElementsByTagName("a");
+//     // alert(links[0])
+//      links[1].onclick = showpic();
+    
+
+
+
+
+
+
+
+// function showpic() {
+
+// let placeholder = document.getElementById('placeholder');
+// placeholder.setAttribute('src','2022-10-08_05-41-27_UTC_3.jpg');
+
+
+// }
+
+
+
+const canvas = document.getElementById("my-canvas");
+const ctx = canvas.getContext("2d");
+
+console.log('test');
